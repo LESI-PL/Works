e1cc3c9cc30d446999d4edf3bc7302861ab84dab
BYteSL <BYteSL@users.noreply.github.com>
2020-04-06 16:12:13 +0100

    Projeto Final ver.1.2

---
+++ AED 2 Projeto Final/main.c
@@ -0,0 +1,21 @@
+/*
+* Author: Yuri Lemos e João Figueiredo
+* Description: Este Programa faz leitura de uma fichero ".txt" e com estes dados recolhidos efetua calculos estatisticos
+* Data: 14 / 3 / 2020 
+* Version: 1.0
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#include <windows.h>
+#include "functions.h"
+#include "structs.h"
+/**
+ *!                                                     Main
+ * */
+int main()
+{
+
+}
---
+++ AED 2 Projeto Final/structs.h
@@ -0,0 +1,19 @@
+/**
+** Author: Yuri Lemos E João Figueiredo
+** Description: Este Programa faz leitura de uma fichero ".txt" e com estes dados recolhidos efetua calculos estatisticos
+** Data: 14 / 3 / 2020 
+* Version: 1.0
+*/
+#ifndef STRUCTS_H_
+#define STRUCTS_H_
+#define MAX 200
+
+/**
+ *!                                                 STRUCTS
+*/
+
+/**
+ *!                                              END OF STRUCTS
+*/
+
+#endif
