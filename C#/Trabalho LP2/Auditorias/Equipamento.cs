124a1c798cb199929315cad35ce124d709efa99f
BYteSL <BYteSL@users.noreply.github.com>
2020-04-24 23:57:50 +0100

    .

---
+++ C#/Trabalho LP2/Auditorias/Equipamento.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auditorias
+{
+    class Equipamento
+    {
+        #region Atributos
+        int codigo;
+        string tipo;
+        string marca;
+        string modelo;
+        DateTime aquisisao;
+        #endregion
+
+
+        #region Construtor
+        /// <summary>
+        /// Construtor por padrao
+        /// </summary>
+        public Equipamento()
+        {
+
+        }
+        /// <summary>
+        /// Construtor por valores indicados
+        /// </summary>
+        /// <param codigo="numero"></param>
+        /// <param tipo="type"></param>
+        /// <param marca="brand"></param>
+        /// <param modelo="model"></param>
+        /// <param data de aquisicao="comprado"></param>
+        public Equipamento(int numero, string type, string brand, string model, DateTime comprado)
+        {
+            codigo = numero;
+            tipo = type;
+            marca = brand;
+            modelo = model;
+            aquisisao = comprado;
+        }
+
+        #endregion
+
+        #region Propriedades 
+
+        public int Codigo
+        {
+
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public string Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+
+        }
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+
+        }
+        public DateTime Aquisicao
+        {
+            get { return aquisisao; }
+            set { aquisisao = value; }
+        }
+
+        #endregion
+
+
+        #region Metodos
+
+        public override string ToString()
+        {
+            return "Equipamento" + this.marca + this.modelo + this.tipo;
+
+        }
+
+        #endregion
+    }
+}
+
+
+
---
+++ C#/Trabalho LP2/Auditorias/Vulnerabilidades.cs
@@ -0,0 +1,72 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="IPCA">
+// Copyright (c) IPCA. All rights reserved.</copyright>
+//-----------------------------------------------------------------------
+// <author>Yuri Lemos</author>
+// <desc> This program do the basics of C#</desc>
+// <Date> 4 / 4 / 2020 </Date>
+// <version>1.0</version>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auditorias
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Vulnerabilidades
+    {
+
+        /*
+         * Vulnerabilidades Contém:
+         *      - Uma lista de vulnerabilidades
+         *      - Quantidade de vulnerabilidades
+         */
+        #region Atributos
+        List<Vulnerabilidade> vulnerabilidades;
+        int qtdVulnerabilidades;
+        #endregion
+
+        #region Construtor
+        public Vulnerabilidades()
+        {
+            vulnerabilidades = new List<Vulnerabilidade>();
+            qtdVulnerabilidades = 0;
+        }
+        #endregion
+
+        #region Propriedades
+        public List<Vulnerabilidade> ListaVulnerabilidades
+        {
+            get { return vulnerabilidades; }
+        }
+        #endregion
+
+        #region Metodos
+        public bool InsereVulnerabilidade(Vulnerabilidade vulnerabilidade)
+        {
+            qtdVulnerabilidades++;
+            vulnerabilidades.Add(vulnerabilidade);
+            return true;
+        }
+
+        public string MostrarDados()
+        {
+            string texto = "";
+            for (int i = 0; i < qtdVulnerabilidades; i++)
+            {
+                texto += "Codigo: " + vulnerabilidades[i].Codigo + "\n\n";
+
+            }
+
+
+            return texto;
+        }
+        #endregion
+    }
+}
